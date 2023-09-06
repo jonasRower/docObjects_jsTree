e46a546e92960d6399f15dda2c1520dbb79787cc
jonasRower <119448235+jonasRower@users.noreply.github.com>
2023-09-06 18:30:18 +0200

    Delete NodeJsTable/server.js

--- NodeJsTable/server.js
+++
@@ -1,7 +0,0 @@
-require("@babel/register")({
-    presets: ["@babel/preset-env"]
-});
-
-//module.exports = require('./index.js')
-module.exports = require('./app.js')
-//module.exports = require('./app2.js')
